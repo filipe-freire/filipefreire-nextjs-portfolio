fa1400514bea5035089ed43ea0c9583a6a64a685
Filipe <filipefreire@protonmail.com>
2021-04-05 19:53:26 +0100

    Improve 404 page

--- pages/404.js
+++ pages/404.js
@@ -4,20 +4,20 @@ import styles from "./404.module.scss";
 const NotFound = () => {
   return (
     <div className={styles.flexContainer}>
-      <div>
-        <h1>Oops...</h1>
-        <h2>
+      <h1 className={styles.title}>Oops...</h1>
+      <div className={styles.content}>
+        <p className={styles.paragraph}>
           You're not supposed to be here!{" "}
-          <span role='img' aria-label=''>
-            👀
+          <span role="img" aria-label="">
+            😱
           </span>
-        </h2>
-        <Link href='/'>
+        </p>
+        <Link href="/">
           <a className={styles.btn}>Head home!</a>
         </Link>
       </div>
       <div className={styles.notFoundImage}>
-        <img src='./404-img.png' alt='' />
+        <img src="./404-img.png" alt="" />
       </div>
     </div>
   );
