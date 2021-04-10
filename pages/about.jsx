2434f4c21b6e0446b2cb3487447f1c9c7d8892d6
Filipe <filipefreire@protonmail.com>
2021-04-11 00:05:32 +0100

    -test removing animations & add padding to tools in about

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,7 @@
 import HeadComp from "../components/HeadComp";
-import { motion } from "framer-motion";
+// import { motion, AnimatePresence } from "framer-motion";
 import styles from "../styles/About.module.scss";
 import { useState } from "react";
-import { AnimatePresence } from "framer-motion";
 
 const helloWorldInstances = [
   "🇵🇹 Olá Mundo!",
@@ -10,7 +9,7 @@ const helloWorldInstances = [
   "🇪🇸 Hola Mundo!",
   "🇸🇪 Hej världen!",
   "🇩🇪 Hallo Welt!",
-  "🇷🇺 Привет мир!"
+  "🇷🇺 Привет мир!",
 ];
 
 const tools = [
@@ -21,7 +20,7 @@ const tools = [
   { name: "NodeJS", imgSrc: "/assets/img/tools/nodejs.png" },
   { name: "MongoDB", imgSrc: "/assets/img/tools/mongodb.svg" },
   { name: "React", imgSrc: "/assets/img/tools/react.png" },
-  { name: "NextJS", imgSrc: "/assets/img/tools/Nextjs-logo.svg" }
+  { name: "NextJS", imgSrc: "/assets/img/tools/Nextjs-logo.svg" },
 ];
 
 const About = () => {
@@ -29,81 +28,82 @@ const About = () => {
 
   function changeLang() {
     setTimeout(() => {
-      return count === helloWorldInstances.length - 1 ? setCount(0) : setCount(count + 1);
+      return count === helloWorldInstances.length - 1
+        ? setCount(0)
+        : setCount(count + 1);
     }, 3000);
   }
 
   return (
     <>
-      <HeadComp title='Contact'></HeadComp>
-      <motion.div
+      <HeadComp title="Contact"></HeadComp>
+      <div
         className={styles.container}
-        initial='hidden'
-        animate='visible'
-        exit={{ opacity: 0 }}
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.2
-            }
-          }
-        }}
+        // initial='hidden'
+        // animate='visible'
+        // exit={{ opacity: 0 }}
+        // variants={{
+        //   hidden: {
+        //     scale: 0.8,
+        //     opacity: 0
+        //   },
+        //   visible: {
+        //     scale: 1,
+        //     opacity: 1,
+        //     transition: {
+        //       delay: 0.2
+        //     }
+        //   }
+        // }}
       >
-        <AnimatePresence exitBeforeEnter>
-          <motion.h1
-            className={styles.title}
-            key={count}
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 0.2 }
-            }}
-            onLoad={changeLang()}
-          >
-            {helloWorldInstances[count]}
-          </motion.h1>
-        </AnimatePresence>
+        {/* <AnimatePresence exitBeforeEnter> */}
+        <h1
+          className={styles.title}
+          // key={count}
+          // initial={{ y: -20, opacity: 0 }}
+          // animate={{ y: 0, opacity: 1 }}
+          // exit={{
+          //   opacity: 0,
+          //   transition: { duration: 0.2 }
+          // }}
+          onLoad={changeLang()}
+        >
+          {helloWorldInstances[count]}
+        </h1>
+        {/* </AnimatePresence> */}
         <div className={styles.flexContainer}>
           <div className={styles.aboutContainer}>
             <img
               className={styles.profileImg}
-              src='/assets/img/profile-pic.jpg'
+              src="/assets/img/profile-pic.jpg"
               alt="Filipe Freire's profile"
             />
-            {/* <p className={`${styles.paragraph} ${styles.name}`}>Filipe Freire</p>
-            <p className={styles.paragraph}>Full Stack Web Developer</p>
-
-            <p className={styles.paragraph}>
-              <span className={styles.location}>Based in:</span> Goteborg, Sweden 🇸🇪
-            </p> */}
           </div>
-          <motion.div className={styles.infoContainer}>
+          <div className={styles.infoContainer}>
             <p className={styles.paragraph}>
-              🙋🏻‍♂️ Hey, I'm Filipe, a Web Developer with a background in music & nutrition, passionate
-              about music, javascript, learning new things and improving people's lives through
-              technology.
+              🙋🏻‍♂️ Hey, I'm Filipe, a Web Developer with a background in music &
+              nutrition, passionate about music, javascript, learning new things
+              and improving people's lives through technology.
             </p>
             <br />
             <p className={styles.paragraph}>
-              👨‍💻 Transitioned into the field of Web Development due to its fast-paced environment
-              and immense learning opportunities.
+              👨‍💻 Transitioned into the field of Web Development due to its
+              fast-paced environment and immense learning opportunities.
             </p>
-          </motion.div>
+          </div>
         </div>
         <p className={styles.toolbelt}>🛠 Toolbelt:</p>
         <div className={styles.tools}>
           {tools.map((tool, i) => (
-            <img className={styles.tool} key={i} src={tool.imgSrc} alt={tool.name} />
+            <img
+              className={styles.tool}
+              key={i}
+              src={tool.imgSrc}
+              alt={tool.name}
+            />
           ))}
         </div>
-      </motion.div>
+      </div>
     </>
   );
 };
